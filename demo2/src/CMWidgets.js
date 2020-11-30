53734161ae7a7234c6f15be076a07b88cc5aca52
codemobiles <chaiyasit.t@gmail.com>
2020-11-30 14:23:13 +0700

    134

--- demo2/src/CMWidgets.js
+++ demo2/src/CMWidgets.js
@@ -13,8 +13,41 @@ const Spacer = (props) => {
   return <View style={{width: props.size}} />;
 };
 
-const CMEntry = () =>{
-    return ()
-}
+const CMEntry = () => {
+  return (
+    <View
+      style={{
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 10,
+        alignItems: 'center',
+        flexDirection: 'row',
+      }}>
+      {/* Icon */}
+      <View
+        style={{
+          height: 30,
+          width: 30,
+          backgroundColor: 'red',
+          borderRadius: 15,
+        }}
+      />
 
-export {Spacer};
+      <Spacer size={8} />
+
+      <TextInput
+        keyboardType="email-address"
+        placeholder="Username"
+        style={{
+          borderColor: '#0003',
+          borderWidth: 1,
+          flex: 1,
+          borderRadius: 10,
+          paddingLeft: 8,
+        }}
+      />
+    </View>
+  );
+};
+
+export {Spacer, CMEntry};
--- demo2/src/RegisterScreen.js
+++ demo2/src/RegisterScreen.js
@@ -64,6 +64,8 @@ export default function RegisterScreen() {
             }}
           />
         </View>
+       
+       
         {/* Password  */}
         <View
           style={{
