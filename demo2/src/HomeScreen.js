f6474875c6cc4df8bc66305d66e9ec60047d611b
codemobiles <chaiyasit.t@gmail.com>
2020-11-30 13:26:09 +0700

    134

--- demo2/src/HomeScreen.js
+++ demo2/src/HomeScreen.js
@@ -27,6 +27,7 @@ export default function HomeScreen() {
             paddingLeft: 10,
             paddingRight: 10,
             paddingTop: 10,
+            alignItems: 'center',
             flexDirection: 'row',
           }}>
           {/* Icon */}
@@ -43,7 +44,46 @@ export default function HomeScreen() {
 
           <TextInput
             placeholder="Username"
-            style={{borderColor: '#0007', borderWidth: 1, flex: 1}}
+            style={{
+              borderColor: '#0003',
+              borderWidth: 1,
+              flex: 1,
+              borderRadius: 10,
+              paddingLeft: 8,
+            }}
+          />
+        </View>
+
+        {/* Password  */}
+        <View
+          style={{
+            paddingLeft: 10,
+            paddingRight: 10,
+            paddingTop: 10,
+            alignItems: 'center',
+            flexDirection: 'row',
+          }}>
+          {/* Icon */}
+          <View
+            style={{
+              height: 30,
+              width: 30,
+              backgroundColor: 'yellow',
+              borderRadius: 15,
+            }}
+          />
+
+          <View style={{width: 8}} />
+
+          <TextInput
+            placeholder="Password"
+            style={{
+              borderColor: '#0003',
+              borderWidth: 1,
+              flex: 1,
+              borderRadius: 10,
+              paddingLeft: 8,
+            }}
           />
         </View>
       </View>
