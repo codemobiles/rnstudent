99a5331f638fcb9c53007d58e4b901f99676eb95
codemobiles <chaiyasit.t@gmail.com>
2020-11-30 14:27:37 +0700

    134

--- demo2/src/RegisterScreen.js
+++ demo2/src/RegisterScreen.js
@@ -9,7 +9,7 @@ import {
   Button,
 } from 'react-native';
 
-import {Spacer} from './CMWidgets';
+import {CMEntry, Spacer} from './CMWidgets';
 
 export default function RegisterScreen() {
   // JSX
@@ -32,73 +32,11 @@ export default function RegisterScreen() {
           paddingBottom: 10,
         }}>
         {/* Username  */}
-        <View
-          style={{
-            paddingLeft: 10,
-            paddingRight: 10,
-            paddingTop: 10,
-            alignItems: 'center',
-            flexDirection: 'row',
-          }}>
-          {/* Icon */}
-          <View
-            style={{
-              height: 30,
-              width: 30,
-              backgroundColor: 'red',
-              borderRadius: 15,
-            }}
-          />
+        <CMEntry />
 
-          <Spacer size={8} />
-
-          <TextInput
-            keyboardType="email-address"
-            placeholder="Username"
-            style={{
-              borderColor: '#0003',
-              borderWidth: 1,
-              flex: 1,
-              borderRadius: 10,
-              paddingLeft: 8,
-            }}
-          />
-        </View>
-       
-       
         {/* Password  */}
-        <View
-          style={{
-            paddingLeft: 10,
-            paddingRight: 10,
-            paddingTop: 10,
-            alignItems: 'center',
-            flexDirection: 'row',
-          }}>
-          {/* Icon */}
-          <View
-            style={{
-              height: 30,
-              width: 30,
-              backgroundColor: 'yellow',
-              borderRadius: 15,
-            }}
-          />
+        <CMEntry />
 
-          <Spacer size={8} />
-
-          <TextInput
-            secureTextEntry
-            placeholder="Password"
-            style={{
-              borderColor: '#0003',
-              borderWidth: 1,
-              flex: 1,
-              borderRadius: 10,
-              paddingLeft: 8,
-            }}
-          />
-        </View>
         {/* Login Button */}
         <TouchableOpacity
           onPress={() => {
