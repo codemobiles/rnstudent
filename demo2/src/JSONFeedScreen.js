52f70744304369107261674287af0fb90e4ee0bd
codemobiles <chaiyasit.t@gmail.com>
2020-12-01 12:02:17 +0700

    1234

--- demo2/src/JSONFeedScreen.js
+++ demo2/src/JSONFeedScreen.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text} from 'react-native';
 
 export default function JSONFeedScreen() {
+  const dataArray = ['Angular', 'RaspberryPi', 'React'];
+
   return (
-    <View>
-      <Text>JSON</Text>
+    <View style={{flex: 1}}>
+      {dataArray.map((item, index) => (
+        <Text key={String(index)}>{item} </Text>
+      ))}
     </View>
-  )
+  );
 }
