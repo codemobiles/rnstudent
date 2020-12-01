10046c36b1e0f92e652680caf567a979f49620d6
codemobiles <chaiyasit.t@gmail.com>
2020-12-01 13:22:27 +0700

    1234

--- demo2/App.js
+++ demo2/App.js
@@ -2,8 +2,16 @@ import React from 'react';
 import {View, Text, StatusBar, SafeAreaView} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import AppNavigator from './src/AppNavigator';
+import AsyncStorage from '@react-native-community/async-storage';
+
 
 export default function App() {
+
+  const [showLogin, setShowLogin] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+
+  
+
   return (
     <NavigationContainer>
       <View style={{flex: 1}}>
