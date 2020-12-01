c0d4f22111e10e5e1e8d1d5b6142e5ec78693f9b
codemobiles <chaiyasit.t@gmail.com>
2020-12-01 13:58:51 +0700

    1234

--- demo2/src/JSONFeedScreen.js
+++ demo2/src/JSONFeedScreen.js
@@ -3,27 +3,26 @@ import {View, Text} from 'react-native';
 import axios from 'axios';
 
 export default function JSONFeedScreen() {
-  
-  const [dataArray, setDataArray] = useState([])
+  const [dataArray, setDataArray] = React.useState([]);
 
   React.useEffect(() => {
     loadData();
   }, []);
 
-  loadData = async () => {
+  const loadData = async () => {
     const regUsername = 'admin';
     const regPassword = 'password';
     const data = `username=${regUsername}&password=${regPassword}&type=foods`;
     const url = 'https://codemobiles.com/adhoc/youtubes/index_new.php';
     const result = await axios.post(url, data);
-
+    setDataArray(result.data.youtubes);
   };
 
   return (
     <View style={{flex: 1}}>
       {dataArray.map((item, index) => (
         <Text key={String(index)}>
-          {index}. {item}
+          {index}. {item.title}
         </Text>
       ))}
     </View>
