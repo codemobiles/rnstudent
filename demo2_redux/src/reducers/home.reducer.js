0488e2e05ff7635708d9c79bf944cd33bda2b2f3
codemobiles <chaiyasit.t@gmail.com>
2020-12-02 15:53:53 +0700

    1234

---
+++ demo2_redux/src/reducers/home.reducer.js
@@ -0,0 +1,36 @@
+import {LOGIN_FAILED, LOGIN_FETCHING, LOGIN_SUCCESS} from '../Constants';
+
+const initialState = {
+  onlineUsername: null,
+  isFetching: false,
+  isFailed: false,
+};
+
+export default (state = initialState, {type, payload}) => {
+  switch (type) {
+    case LOGIN_FETCHING:
+      return {
+        ...state,
+        onlineUsername: null,
+        isFetching: true,
+        isFailed: false,
+      };
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        onlineUsername: payload,
+        isFetching: false,
+        isFailed: false,
+      };
+    case LOGIN_FAILED:
+      return {
+        ...state,
+        onlineUsername: null,
+        isFetching: false,
+        isFailed: true,
+      };
+
+    default:
+      return state;
+  }
+};
