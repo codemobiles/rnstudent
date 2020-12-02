d09d7bc2c65d81e1c1cbd122a6a6fca5d65657b6
codemobiles <chaiyasit.t@gmail.com>
2020-12-02 09:31:41 +0700

    1234

--- demo2/backend/server.js
+++ demo2/backend/server.js
@@ -1,2 +1,14 @@
-// npx nodemon server.js
-console.log('Hello ddd');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.end('I am home');
+});
+
+app.get('/login', (req, res) => {
+  res.end('I am login');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running.');
+});
