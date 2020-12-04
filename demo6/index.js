fd9f32365e391106b7b6c1c5defd4a9f48aae706
codemobiles <chaiyasit.t@gmail.com>
2020-12-04 11:48:46 +0700

    2134

--- demo6/index.js
+++ demo6/index.js
@@ -1,22 +1,24 @@
 /**
  * @format
  */
+import React from 'react';
 
 import {AppRegistry} from 'react-native';
-import { createStore , applyMiddleware} from 'redux';
-import {Provider} from "react-redux"
 import App from './App';
-import {name as appName} from './app.json';
-import reducers from "./src/reducers"
-import thunk from "redux-thunk"
-import logger from "redux-logger"
 
-const store = createStore(reducers, applyMiddleware(thunk, logger))
+import {createStore, applyMiddleware} from 'redux';
+import {Provider} from 'react-redux';
+import {name as appName} from './app.json';
+import reducers from './src/reducers';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+const store = createStore(reducers, applyMiddleware(thunk, logger));
 
 const ReduxApp = () => (
-    <Provider store={store}>
-      <App />
-    </Provider>
-  );
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 AppRegistry.registerComponent(appName, () => ReduxApp);
