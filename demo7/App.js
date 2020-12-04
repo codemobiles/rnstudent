068a194257c3c611d5c0e99fd83a36235ff2e0d0
codemobiles <chaiyasit.t@gmail.com>
2020-12-04 14:34:19 +0700

    2134

--- demo7/App.js
+++ demo7/App.js
@@ -11,12 +11,13 @@ export default function App() {
 
   const loadData = async ()=>{
     const result = await axios.get("https://codemobiles.com/adhoc/youtubes/index_new.php?username=admin&password=password&type=foods")
+    setDataArray(result.data.youtubes)
   }
 
 
   return (
-    <View>
-      <Text></Text>
+    <View style={{flex:1}}>
+      {dataArray.map(item=>(<Text key={item.id}>{item.title}</Text>))} 
     </View>
   )
 }
